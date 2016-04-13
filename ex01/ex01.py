f7d9984d6862f54bb868231c4bba55264daff24d
CAD Client <CAD Client>
2016-04-13 18:41:30 +0900

    ex01

---
+++ ex01/ex01.py
@@ -0,0 +1,11 @@
+print"hello world"
+print"Hello Again"
+print"I like typig this."
+print"This is fun."
+print" Yay! printing."
+print"I'd much rather you 'not"
+print'i "said" do not touch this.'
+
+print("goen hui")
+print("2016 03 09 19:49")
+print("Lee Se Dol : Alpha Go = 0 :1")
